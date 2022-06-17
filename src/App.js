6aceeee51d8289c1b7cc59310b4373543c40e4d6
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-06-16 21:39:48 -0300

    criaçao e estilizaçao do componente ROW, adiçao parametro isLarge api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import Row from './components/Row';
+import categories from './api';
 
 function App() {
   return (
     <div className="App">
-      <h1>teste</h1>
       {/* Navbar */}
       {/* Destaques */}
-      {/* Em alta */}
       {/* Filmes de cada categoria */}
+      {categories.map((category) => {
+        return (
+          <Row
+            key={category.name}
+            title={category.title}
+            path={category.path}
+            isLarge={category.isLarge}
+          />
+        );
+      })}
     </div>
   );
 }
